9acc7918f731a372ed8d87890eae5d3234efd45b
Mateja <markojovanovicc23@outlook.com>
2026-05-13 14:43:41 +0200

    changed from svg to avif images

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     <Navbar/>
     <main className="min-h-screen flex flex-col items-center justify-center py-20">
       <div className="text-center text-5xl">
-        <h1 className="hero-text-animation">Dostojanstveno, <span className={`bg-gray-200 px-2 ${libre.className}`}>bez suvišnog.</span><br />Mir u trenucima oproštaja.</h1>
+        <h1>Dostojanstveno, <span className={`bg-gray-200 px-2 ${libre.className}`}>bez suvišnog.</span><br />Mir u trenucima oproštaja.</h1>
       </div>
       <div className="mt-8 flex items-center hero-text-animation">
         <Link href="/kontakt" className="bg-black text-white px-12 py-4 text-sm rounded-full">
@@ -33,27 +33,27 @@ export default function Home() {
     <section className="hidden mt-20 lg:flex items-start justify-center gap-4 px-4 max-w-5xl mx-auto">
       <div className="w-32 md:w-40 mt-16">
         <div className="relative h-40 md:h-55 rounded-2xl overflow-hidden">
-          <Image src="/img1.svg" alt="" fill className="object-cover" />
+          <Image src="/img1.avif" alt="" fill className="object-cover" />
         </div>
       </div>
       <div className="w-32 md:w-40 mt-8">
         <div className="relative h-44 md:h-60 rounded-2xl overflow-hidden">
-          <Image src="/img2.svg" alt="" fill className="object-cover" />
+          <Image src="/img2.avif" alt="" fill className="object-cover" />
         </div>
       </div>
       <div className="w-36 md:w-44 mt-0">
         <div className="relative h-52 md:h-65 rounded-2xl overflow-hidden">
-          <Image src="/img3.svg" alt="" fill className="object-cover" />
+          <Image src="/img3.avif" alt="" fill className="object-cover" />
         </div>
       </div>
       <div className="w-32 md:w-40 mt-8">
         <div className="relative h-44 md:h-60 rounded-2xl overflow-hidden">
-          <Image src="/img4.svg" alt="" fill className="object-cover" />
+          <Image src="/img4.avif" alt="" fill className="object-cover" />
         </div>
       </div>
       <div className="w-32 md:w-40 mt-16">
         <div className="relative h-40 md:h-55 rounded-2xl overflow-hidden">
-          <Image src="/img5.svg" alt="" fill className="object-cover" />
+          <Image src="/img5.avif" alt="" fill className="object-cover" />
         </div>
       </div>
     </section>
@@ -61,10 +61,10 @@ export default function Home() {
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
         <div className="relative">
           <div className="relative h-125 w-full md:w-[90%] overflow-hidden">
-            <Image src="/cvece.svg"  alt="" fill className="object-cover rounded-lg" />
+            <Image src="/cvece.svg"  alt="" fill className="object-cover rounded-lg" loading="lazy"/>
           </div>
           <div className="absolute -bottom-10 right-0 md:right-4 w-64 h-64 border-5 border-white rounded-[30px] overflow-hidden shadow-xl">
-            <Image src="/rukovanje.svg"  alt="" fill className="object-cover" />
+            <Image src="/rukovanje.svg"  alt="" fill className="object-cover" loading="lazy"/>
           </div>
         </div>
         <div className="space-y-6 lg:pl-12">
@@ -104,53 +104,57 @@ export default function Home() {
         <div className="grid grid-cols-1 md:grid-cols-12 gap-8 items-stretch">
           <div className="md:col-span-7 group bg-[#f9f7f4] rounded-lg overflow-hidden transition-all duration-500 flex-1">
             <div className="relative h-64 md:h-80 w-full">
-              <Image  src="/usluga1.svg"  alt="Pogrebne Usluge" fill className="object-cover transition-transform duration-700 group-hover:scale-105" />
+              <Image  src="/usluga1.avif" alt="Pogrebne Usluge" fill className="object-cover transition-transform duration-700 group-hover:scale-105" loading="lazy"/>
             </div>
             <div className="p-8 md:p-10">
               <h3 className="text-2xl font-serif text-[#2a2a2a] mb-4">Pogrebne Usluge</h3>
               <p className="text-gray-500 text-[13px] leading-relaxed mb-8 opacity-80">
                 Sveobuhvatna organizacija koja uključuje tradicionalna izlaganja, verske obrede i savremene ceremonije života u kapeli.</p>
-              <button className="flex items-center text-[0.625rem] font-bold tracking-[0.15em] text-[#555] hover:text-black transition-colors uppercase">
-                PROČITAJ VIŠE <ArrowRight className="ml-2 w-4 h-4" /></button>
+              <Link href="/" className="flex items-center text-[0.625rem] font-bold tracking-[0.15em] text-[#555] hover:text-black transition-colors uppercase">
+                PROČITAJ VIŠE <ArrowRight className="ml-2 w-4 h-4" />
+              </Link>
             </div>
           </div>
           <div className="md:col-span-5 group bg-[#f9f7f4] rounded-lg overflow-hidden transition-all duration-500 flex-1">
             <div className="relative h-64 md:h-80 w-full">
-              <Image  src="/usluga2.svg"  alt="Iznos i Prevoz Pokojnika" fill className="object-cover transition-transform duration-700 group-hover:scale-105" />
+              <Image  src="/usluga2.avif" alt="Iznos i Prevoz Pokojnika" fill className="object-cover transition-transform duration-700 group-hover:scale-105" loading="lazy"/>
             </div>
             <div className="p-8 md:p-10">
               <h3 className="text-2xl font-serif text-[#2a2a2a] mb-4">Iznos i Prevoz Pokojnika</h3>
               <p className="text-gray-500 text-[13px] leading-relaxed mb-8 opacity-80">
                 Siguran prevoz pokojnika iz zemlje i inostranstva, uz organizaciju i povoljne cene.
               </p>
-              <button className="flex items-center text-[0.625rem] tracking-[0.15em] font-bold text-[#555] hover:text-black transition-colors uppercase">
-                PROČITAJ VIŠE <ArrowRight className="ml-2 w-4 h-4" /></button>
+              <Link href="/" className="flex items-center text-[0.625rem] tracking-[0.15em] font-bold text-[#555] hover:text-black transition-colors uppercase">
+                PROČITAJ VIŠE <ArrowRight className="ml-2 w-4 h-4" />
+              </Link>
             </div>
           </div>
           <div className="md:col-span-5 group bg-[#f9f7f4] rounded-lg overflow-hidden transition-all duration-500 flex-1">
             <div className="relative h-64 md:h-80 w-full">
-              <Image  src="/usluga3.svg"  alt="Pogrebna oprema" fill className="object-cover transition-transform duration-700 group-hover:scale-105" />
+              <Image  src="/usluga3.avif" alt="Pogrebna oprema" fill className="object-cover transition-transform duration-700 group-hover:scale-105" loading="lazy"/>
             </div>
             <div className="p-8 md:p-10">
               <h3 className="text-2xl font-serif text-[#2a2a2a] mb-4">Pogrebna oprema</h3>
               <p className="text-gray-500 text-[13px] leading-relaxed mb-8 opacity-80">
                 Kvalitetna oprema za dostojanstven ispraćaj, veliki izbor sanduka, cveća i prateće opreme.
               </p>
-              <button className="flex items-center text-[0.625rem] font-bold tracking-[0.15em] text-[#555] hover:text-black transition-colors uppercase">
-                PROČITAJ VIŠE <ArrowRight className="ml-2 w-4 h-4" /> </button>
+              <Link href="/" className="flex items-center text-[0.625rem] font-bold tracking-[0.15em] text-[#555] hover:text-black transition-colors uppercase">
+                PROČITAJ VIŠE <ArrowRight className="ml-2 w-4 h-4" />
+              </Link>
             </div>
           </div>
           <div className="md:col-span-7 group bg-[#f9f7f4] rounded-lg overflow-hidden transition-all duration-500 flex-1">
             <div className="relative h-64 md:h-80 w-full">
-              <Image  src="/usluga4.svg"  alt="Administrativna pomoć" fill className="object-cover transition-transform duration-700 group-hover:scale-105" />
+              <Image  src="/usluga4.avif"  alt="Administrativna pomoć" fill className="object-cover transition-transform duration-700 group-hover:scale-105" loading="lazy"/>
             </div>
             <div className="p-8 md:p-10">
               <h3 className="text-2xl font-serif text-[#2a2a2a] mb-4">Administrativna pomoć</h3>
               <p className="text-gray-500 text-[13px] leading-relaxed mb-8 opacity-80">
                 Stručna administrativna pomoć pri organizaciji sahrane i rešavanju potrebne dokumentacije.
               </p>
-              <button className="flex items-center text-[0.625rem] font-bold tracking-[0.15em] text-[#555] hover:text-black transition-colors uppercase">
-                PROČITAJ VIŠE <ArrowRight className="ml-2 w-4 h-4" /></button>
+              <Link href="/" className="flex items-center text-[0.625rem] font-bold tracking-[0.15em] text-[#555] hover:text-black transition-colors uppercase">
+                PROČITAJ VIŠE <ArrowRight className="ml-2 w-4 h-4" />
+              </Link>
             </div>
           </div>
         </div>
