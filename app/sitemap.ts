83ce0f2db677030f1f49d2adbd48af9f78b9c48d
Mateja <markojovanovicc23@outlook.com>
2026-05-13 19:19:48 +0200

    added favicon.ico added metadata added robots.ts added sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+﻿import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://rajskekocije.rs',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://rajskekocije.rs/onama',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.9,
+        },
+        {
+            url: 'https://rajskekocije.rs/kontakt',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.9,
+        },
+    ]
+}
