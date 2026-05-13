168f1da9b76f4616245684f3b4ff193eb27eebee
Mateja <markojovanovicc23@outlook.com>
2026-05-13 15:06:37 +0200

    fixed accessability issue in Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,10 +31,12 @@ function Navbar() {
                 <Link href="/" className={`${libre.className} text-lg w-50 flex z-100`}>Rajske Kočije</Link>
                 <ul className="hidden lg:flex flex-row gap-10 w-full items-center justify-center">
                     {links.map((link: Links, index: number) => (
-                        <Link key={index} href={link.href} className="text-black uppercase hover:underline underline-offset-4">{link.name}</Link>
+                        <li key={index}>
+                            <Link href={link.href} className="text-black uppercase hover:underline underline-offset-4">{link.name}</Link>
+                        </li>
                     ))}
                 </ul>
-                <Link href="/kontakt" className="uppercase underline w-75 hidden lg:block">kontakt</Link>
+                <Link href="/kontakt" className="uppercase underline underline-offset-4 w-75 hidden lg:block">kontakt</Link>
                 <button aria-label="Otvori meni" className="flex z-11 lg:hidden" onClick={(prevState) => setActiveSidebar((prevState) => !prevState)}>
                     {
                         activeSidebar ? <X/> : <Menu/>
@@ -44,7 +46,9 @@ function Navbar() {
             <div className={`w-full min-h-screen fixed bg-white z-99 flex flex-col transition-all duration-300 ${activeSidebar ? "translate-x-0" : "translate-x-full"}`}>
                 <ul className="w-full h-[60vh] flex flex-col gap-5 justify-center items-center">
                     {links.map((link: Links, index: number) => (
-                        <li key={index} className="text-black underline underline-offset-4 uppercase">{link.name}</li>
+                        <li key={index}>
+                            <Link href={link.href} className="text-black underline underline-offset-4 uppercase">{link.name}</Link>
+                        </li>
                     ))}
                     <li><Link href="/kontakt" className="uppercase underline text-black">kontakt</Link></li>
                 </ul>
