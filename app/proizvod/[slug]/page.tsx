4068f4b7b93619ed22c433a1c99857700d865472
matkethekid <markojovanovicc23@outlook.com>
2026-05-30 22:49:34 +0200

    added /proizvod route with slug

---
+++ app/proizvod/[slug]/page.tsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import IndividualItem from "@/components/IndividualItem";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+function page() {
+    return (
+        <div className="relative w-full min-h-screen">
+            <Navbar/>
+            <IndividualItem/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default page;
