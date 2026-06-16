8c750d4e22ab5e7dd08ae4e6b58105e5673367c0
matkethekid <markojovanovicc23@outlook.com>
2026-06-16 08:21:12 +0200

    feat: add new urls to sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -20,5 +20,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: 'weekly',
             priority: 0.9,
         },
+        {
+            url: 'https://rajskekocije.rs/pogrebna-oprema',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.9,
+        },
+        {
+            url: 'https://rajskekocije.rs/pogrebne-usluge',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.9,
+        },
     ]
 }
